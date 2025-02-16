1dad93c577c583b03babbf375c5be90c1b3e8f2f
greateng <10429483+greateng@users.noreply.github.com>
2025-02-16 22:47:20 +0800

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Media Editor</h1>
+      <p>Welcome to the self-hosted media editing tool!</p>
+    </div>
+  );
+}
+
+export default App;
